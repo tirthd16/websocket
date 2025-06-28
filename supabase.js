c0bc96aaa1b6f6c6fe54ad82568642679a34d9b0
Tirth Dudhat <tirth1644@gmail.com>
2025-06-28 12:47:29 +0530

    DEBUG: public env variable

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log(process.env.SUPABASE_URL)
-console.log("heretirth")
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // must use service role
+  process.env.PUBLIC_SUPABASE_URL,
+  process.env.PUBLIC_SUPABASE_SERVICE_ROLE_KEY // must use service role
 )
